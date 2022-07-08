f622d648954b5ac25633990df0227226d4a73152
hineko <cnxnd11@gmail.com>
2022-07-08 15:13:09 +0300

    caesar decoder

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from art import text2art
 import string
 
 def engBruteforceDecrypt(enc_string):
-    LettersAndDigits = string.ascii_letters + string.digits + string.punctuation #lowercase and uppercase eng aphabet + spec. symbols
-    print("ROT RIGHT") # Shift right(exp. +i shifts) 
-    for shift in range(len(enc_string)):
+    LettersAndDigits = string.ascii_letters + string.digits + string.punctuation # lowercase and uppercase eng aphabet + spec. symbols
+    print("ROT RIGHT") # Shift right(exp. +i shifts)
+    for shift in range(len(LettersAndDigits)):
         encoded = ""
         for i in enc_string:
             if i in LettersAndDigits:
@@ -20,7 +20,7 @@ def engBruteforceDecrypt(enc_string):
         print(f"Shift {shift}: {encoded}")
 
     print("ROT LEFT")# Shift left(exp. -i shifts)
-    for shift in range(len(enc_string)):
+    for shift in range(len(LettersAndDigits)):
         encoded = ""
         for i in enc_string:
             if i in LettersAndDigits:
@@ -37,7 +37,7 @@ def engBruteforceDecrypt(enc_string):
 def rusBruteforceDecrypt(enc_string):
     LettersAndDigits = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ" # russian alphabet
     print("ROT RIGHT")
-    for shift in range(len(enc_string)):
+    for shift in range(len(LettersAndDigits)):
         encoded = ""
         for i in enc_string:
             if i in LettersAndDigits:
@@ -50,7 +50,7 @@ def rusBruteforceDecrypt(enc_string):
                 encoded += i
         print(f"Shift {shift}: {encoded}")
     print("ROT LEFT")
-    for shift in range(len(enc_string)):
+    for shift in range(len(LettersAndDigits)):
         encoded = ""
         for i in enc_string:
             if i in LettersAndDigits:
@@ -66,8 +66,13 @@ def rusBruteforceDecrypt(enc_string):
 if __name__ == '__main__':
     print(text2art("Caesar cipher decoder"))
     inputLang = input("RUS(1) or ENG(2) string: ")
-    inputString = input("Input string: ")
-    if inputLang in ("1" or "rus" or "RUS"):
-        rusBruteforceDecrypt(inputString)
-    else:
-        engBruteforceDecrypt(inputString)
+    while True:
+        if inputLang in ("1" or "rus" or "RUS"):
+            inputString = input("Input string: ")
+            rusBruteforceDecrypt(inputString)
+        elif inputLang in ("2" or "eng" or "ENG"):
+            inputString = input("Input string: ")
+            engBruteforceDecrypt(inputString)
+        else:
+            print("Incorrect input")
+            inputLang = input("RUS(1) or ENG(2) string: ")
